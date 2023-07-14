9a8179730e5604b997860b3b1a02b71d0f2eb4db
Squeezik <mrsqueezeme@gmail.com>
2023-07-14 11:31:30 +0200

    Filter Method

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -273,3 +273,13 @@ const essentialData = books.map((book) =>
   })
 );
 essentialData;
+
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes('adventure'))
+  .map((book) => book.title);
+adventureBooks;
