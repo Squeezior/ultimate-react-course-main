6d10dfd3eb4bc816ab8c87f308a81db0ef0a247e
Squeezik <mrsqueezeme@gmail.com>
2023-11-28 13:27:40 +0100

    Sorting and clear functions added in PackingList

--- 04-steps/Coding-Challenge-Counter/coding-challenge-counter-v1/src/App.js
+++ 04-steps/Coding-Challenge-Counter/coding-challenge-counter-v1/src/App.js
@@ -16,13 +16,6 @@ function Counter() {
   const date = new Date('June 21 2027');
   date.setDate(date.getDate() + count);
 
-  function nextStepHandler() {
-    setStep((s) => s + 1);
-  }
-  function previousStepHandler() {
-    setStep((s) => s - 1);
-  }
-
   function nextCountHandler() {
     setCount((c) => c + step);
   }
@@ -30,16 +23,36 @@ function Counter() {
     setCount((c) => c - step);
   }
 
+  const stepHandler = (e) => {
+    setStep(Number(e.target.value));
+  };
+
+  const inputDaysHandler = (e) => {
+    setCount(Number(e.target.value));
+  };
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <div>
       <div>
-        <button onClick={previousStepHandler}>-</button>
+        <input
+          type='range'
+          min='0'
+          max='10'
+          step='1'
+          value={step}
+          onChange={stepHandler}
+        ></input>
+
         <span>Step: {step} </span>
-        <button onClick={nextStepHandler}>+</button>
       </div>
       <div>
         <button onClick={previousCountHandler}>-</button>
-        <span>Count: {count}</span>
+        <input type='text' value={count} onChange={inputDaysHandler}></input>
         <button onClick={nextCountHandler}>+</button>
       </div>
       <span>{count === 0 && `Today is ${date.toDateString()}`}</span>
@@ -47,6 +60,12 @@ function Counter() {
         {count > 0 && `${count} days from today is ${date.toDateString()}`}
       </span>
       <span>{count < 0 && `${count} days ago was ${date.toDateString()}`}</span>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
